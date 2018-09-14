c88e7bd084e8ba0a8ccfb692e6f2883cfb623c07
Gautham Nookala <gnookala@central1.com>
2018-09-14 12:14:00 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view_engine', hbs);
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.')
+    }
+  })
+  next()
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+app.get('', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Some text here...'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad, bad, bad!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
